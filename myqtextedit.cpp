17b406831031430eb83f1dd3671f3054747695d7
Matteo <matteo.stoisa@gmail.com>
2020-10-20 10:52:13 +0200

    default style fix

--- myqtextedit.cpp
+++ myqtextedit.cpp
@@ -1,6 +1,13 @@
 #include "myqtextedit.h"
 #include <QMimeData>
 
+#define DEFAULT_FONT_FAMILY "Helvetica"
+#ifdef __linux__
+    #define DEFAULT_FONT_SIZE 11
+#else //winzoz
+    #define DEFAULT_FONT_SIZE 9
+#endif
+
 //bool MyQTextEdit::canInsertFromMimeData(const QMimeData *source) const{
 //    if(source->hasText()){
 //        return true;
@@ -14,11 +21,12 @@ void MyQTextEdit::insertFromMimeData(const QMimeData *source){
         QTextCursor cursor = textCursor();
 
         QTextCharFormat fmt;
-        fmt.setFontFamily("Sans Serif");
+        fmt.setFontFamily(DEFAULT_FONT_FAMILY);
         fmt.setFontItalic(false);
         fmt.setFontUnderline(false);
         fmt.setFontWeight(QFont::Normal);
-        fmt.setFontPointSize(11);
+        fmt.setFontPointSize(DEFAULT_FONT_SIZE);
+        fmt.setForeground(QColor(QString::fromStdString("#000000")));
         cursor.setCharFormat(fmt);
         cursor.insertText(text);
 
